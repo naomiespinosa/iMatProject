3a5efec8b7e015fba71923146d23a58190604807
Naomi Espinosa <naomisespinosa@gmail.com>
2019-05-07 17:02:51 +0200

    ändrat lite i projektstrukturen

--- iMatProject/src/iMatProject/iMatMainWindowController.java
+++ iMatProject/src/iMatProject/iMatMainWindowController.java
@@ -1,6 +1,6 @@
 package iMatProject;
 
-public class iMatWindowController {
+public class iMatMainWindowController {
 //test, denna fil ska inte ha en main
     public static void main(String [ ] args)
     {
