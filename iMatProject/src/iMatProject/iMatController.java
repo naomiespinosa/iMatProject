23291fea163c78371f002e1df45ad157a6c56141
Naomi Espinosa <naomisespinosa@gmail.com>
2019-05-07 16:49:34 +0200

    ändrat lite i projektstrukturen

--- iMatProject/src/iMatProject/iMatController.java
+++ iMatProject/src/iMatProject/iMatController.java
@@ -1,7 +1,7 @@
 package iMatProject;
 
 public class iMatController {
-//test, ska inte vara main eller runnable
+//test, denn fil ska inte ha en main
     public static void main(String [ ] args)
     {
         System.out.print(System.getProperty("user.home"));
