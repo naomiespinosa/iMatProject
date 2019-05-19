ae54ed05b024101bd82ebcb79898eb5cf8b9a274
MIDO-96 <melhemm@student.chalmers.se>
2019-05-19 18:16:23 +0200

    Fixed coloring of the favorites Button.

--- iMatProject/src/iMatProject/ProductPanel.java
+++ iMatProject/src/iMatProject/ProductPanel.java
@@ -4,6 +4,7 @@ package iMatProject;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
@@ -16,24 +17,28 @@ import se.chalmers.cse.dat216.project.ShoppingItem;
 
 public class ProductPanel extends AnchorPane {
 
-    @FXML ImageView imageView;
-    @FXML Label nameLabel;
-    @FXML Label prizeLabel;
-    @FXML Label ecoLabel;
-    @FXML Button favoriteButton;
-    
+    @FXML
+    ImageView imageView;
+    @FXML
+    Label nameLabel;
+    @FXML
+    Label prizeLabel;
+    @FXML
+    Label ecoLabel;
+    @FXML
+    Button favoriteButton;
+
     private Model model = Model.getInstance();
 
     private Product product;
     private ShoppingItem item;
-    
+
     private final static double kImageWidth = 75;
     private final static double kImageRatio = 0.75;
 
     public ProductPanel(Product product) {
 
 
-        
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ProductPanel.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
@@ -46,13 +51,10 @@ public class ProductPanel extends AnchorPane {
 
         this.product = product;
         nameLabel.setText(product.getName());
-        prizeLabel.setText(String.format("%.2f", product.getPrice()) + " " + product.getUnit()+ "");
-        imageView.setImage(model.getImage(product, kImageWidth, kImageWidth*kImageRatio));
+        prizeLabel.setText(String.format("%.2f", product.getPrice()) + " " + product.getUnit() + "");
+        imageView.setImage(model.getImage(product, kImageWidth, kImageWidth * kImageRatio));
         if (!product.isEcological()) {
             ecoLabel.setText("");
-        }
-        if (Model.getInstance().isFavorite(product)){
-            favoriteButton.setStyle("-fx-background-color: #4c1036;");
         }
 
 
@@ -64,20 +66,25 @@ public class ProductPanel extends AnchorPane {
         System.out.println("Add " + product.getName());
         model.addToShoppingCart(product);
     }
+
     @FXML
     private void handleRemoveAction(ActionEvent event) {
         System.out.println("Remove " + product.getName());
         model.removeFromShoppingCart(product);
     }
+
     @FXML
-    public  void addFavoritesAction(){
-        if (!model.isFavorite(product)){
+    public void addFavoritesAction() {
+        if (!model.isFavorite(product)) {
             model.addFavorites(product);
 
-    }
-        else {
+        } else {
             model.removeFavorites(product);
-            System.out.println( "Remove fav:  " + product.getProductId());
+            System.out.println("Remove fav:  " + product.getProductId());
         }
-}
+        if (Model.getInstance().isFavorite(product)) {
+            favoriteButton.setStyle("-fx-background-color: #4c1036;");
+        }
+        else favoriteButton.setStyle("-fx-background-color: #ffffff;");
+    }
 }
