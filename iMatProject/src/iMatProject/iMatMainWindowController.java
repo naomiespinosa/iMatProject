ce5efe294c5b3a4e3271856372b684e45fafe6c2
MIDO-96 <melhemm@student.chalmers.se>
2019-05-20 15:09:31 +0200

    the cart is working ;)

--- iMatProject/src/iMatProject/iMatMainWindowController.java
+++ iMatProject/src/iMatProject/iMatMainWindowController.java
@@ -317,6 +317,7 @@ public void showCategoryOnClick(MouseEvent mouseEvent){
         wizardHandler();
 }
 
+
     @FXML
     public void toCartNavigationAction(ActionEvent event){
         toCartNavigation();
